6ce169e6d665535fbe80b9ecd1335a1eb0a1999a
AlwinaO <aoyewole2586@gmail.com>
2019-01-07 22:20:35 -0500

    add post '/login' route and test in browser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,16 @@ class UsersController < ApplicationController
 
   # Route for logging in
   get '/login' do
-    erb :login 
+    erb :login
 
+  end
+
+  # receive the login the login form, find the user and log the user in (create a session)
+  post '/login' do
+    # find the user
+    # authenticate the user
+    # log the user in
+    # redirect to the user's landing page (show? index?  dashboard?)
   end
 
   # Route for  signup
