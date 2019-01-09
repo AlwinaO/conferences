bb6c8dda70d44dde694f7e19473e4b5f34bf129b
AlwinaO <aoyewole2586@gmail.com>
2019-01-08 22:00:42 -0500

    build control flow to find, authenticate and log in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,22 @@ class UsersController < ApplicationController
   # receive the login the login form, find the user and log the user in (create a session)
   post '/login' do
     # find the user
-    # authenticate the user
-    # log the user in
+    @user = User.find_by(email: params[:email])
+    # authenticate the user - verify the user by email/pw combo
+    if @user.authenticate(params[:password])
+    # log the user in - create the session
     # redirect to the user's landing page (show? index?  dashboard?)
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+    # tell the user they entered invalid credentials
+    # redirect to the login page
+    end
+  end
+
+  get '/users/:id' do
+    "this is the user's show page"
+
   end
 
   # Route for  signup
