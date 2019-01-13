4739eeded2647c4fa50c46a65322f858a3388214
AlwinaO <aoyewole2586@gmail.com>
2019-01-12 23:17:43 -0500

    update new route and add logic for post conferences route

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,15 +1,24 @@
 class ConferencesController < ApplicationController
 
   # get conferences/new to render a form to create a new conference
-  get 'conferences/new' do
+  get '/conferences/new' do
     erb :'/conferences/new'
   end
 
   # post conferences to create a new conference
   post '/conferences' do
-    # redirect "/conferences/#{@conference.id}"
-  end
+    # raise params.inspect
+    # {"name"=>"Codeland", "location"=>"New York", "category"=>"Code Newbies", "date"=>"5/5/2019"}
+    # create the entry if a user is logged in
+    if !logged_in?
+      redirect '/'
+    # save the entry if it has a name, location, category and date
+    elsif params[:name] != "" && params[:location] != "" && params[:category] != "" && params[:date] != ""
+      @conference = Conference.create(params)
+      redirect "/conferences/#{@conference.id}"
+    end
 
+  end
 
   # show route for one conference
   get '/conferences/:id' do
