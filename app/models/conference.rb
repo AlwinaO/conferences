dbbb6838ae5129ec7c3e65faecda8a61c992ba10
AlwinaO <aoyewole2586@gmail.com>
2019-01-27 16:34:03 -0500

    comment out conference_date method until further refactoring

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -2,7 +2,7 @@ class Conference < ActiveRecord::Base
 
   belongs_to :user
 
-  def conference_date
-    self.date.strftime("%A %B %e %Y")
-  end
+  # def conference_date
+  #   self.date.strftime("%A %B %e %Y")
+  # end
 end
