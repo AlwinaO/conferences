7b799e7f516934415be79f6f690c3af3b0f2adc9
AlwinaO <aoyewole2586@gmail.com>
2019-01-30 22:18:44 -0500

    edit date to standardize view and future entries by users

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,18 +21,18 @@ tchalla = User.create(name: "T'Challa", email: "tchalla@bp.com", password: "waka
 
 #create conferences
 
-Conference.create(name: "Grace Hopper Celebration", date: "10-2-2019", category: "women", location: "Orlando, FL", user_id: alice.id)
+Conference.create(name: "Grace Hopper Celebration", date: "2019-10-02", category: "women", location: "Orlando, FL", user_id: alice.id)
 
-oscar.conferences.create!(name: "O'Reilly Velocity Conf", date: "6-10-2019", category: "coding", location: "San Jose, CA")
+oscar.conferences.create!(name: "O'Reilly Velocity Conf", date: "2019-06-10", category: "coding", location: "San Jose, CA")
 
-Conference.create(name: "Dash by Datadog", date: "7-10-2019", category: "coding", location: "NYC", user_id: tchalla.id)
+Conference.create(name: "Dash by Datadog", date: "2019-07-10", category: "coding", location: "NYC", user_id: tchalla.id)
 
-sharell.conferences.create!(name: "SXSW", date: "3-18-2019", category: "mixed", location: "Austin, TX")
+sharell.conferences.create!(name: "SXSW", date: "2019-03-18", category: "mixed", location: "Austin, TX")
 
-Conference.create(name: "Red Hat Summit", date: "5-7-2019", category: "open source", location: "San Francisco, CA", user_id: danny.id)
+Conference.create(name: "Red Hat Summit", date: "2019-05-07", category: "open source", location: "San Francisco, CA", user_id: danny.id)
 
-lauren.conferences.create!(name: "JupyterCon", date: "8-19-2019", category: "coding", location: "NYC")
+lauren.conferences.create!(name: "JupyterCon", date: "2019-08-19", category: "coding", location: "NYC")
 
-Conference.create(name: "Tech Inclusion", date: "11-26-2018", category: "diversity", location: "NYC", user_id: matthew.id)
+Conference.create(name: "Tech Inclusion", date: "2018-11-26", category: "diversity", location: "NYC", user_id: matthew.id)
 
-michelle.conferences.create!(name: "Afrotech", date: 11-10-2018, category: "POC", location: "San Francisco, CA")
+michelle.conferences.create!(name: "Afrotech", date: "2019-11-10", category: "POC", location: "San Francisco, CA")
